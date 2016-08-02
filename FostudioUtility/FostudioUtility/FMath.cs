80fe53e903eeea0e09c9dc32fbc7de320de2e315
Foskill <zhangjia517@hotmail.com>
2016-08-02 18:20:07 +0800

    Init

---
+++ FostudioUtility/FostudioUtility/FMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FostudioUtility
+{
+    public static class FMath
+    {
+        /// <summary>
+        /// 四舍五入
+        /// </summary>
+        /// <param name="a"> </param>
+        public static double Round(double a)
+        {
+            return (Math.Round(a));
+        }
+    }
+}
